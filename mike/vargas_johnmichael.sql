fb4e7d34df4313e836fe6e8fdfc91e6e6fa947e8
John Michael E. Vargas <129486648+JMichaelVargas@users.noreply.github.com>
2024-07-11 14:10:10 +0800

    Add files via upload

---
+++ mike/vargas_johnmichael.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 11, 2024 at 08:09 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vargas_johnmichael`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vargas_johnmichael`
+--
+
+CREATE TABLE `vargas_johnmichael` (
+  `id` int(11) NOT NULL,
+  `name` varchar(150) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `email` varchar(150) NOT NULL,
+  `password` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `vargas_johnmichael`
+--
+
+INSERT INTO `vargas_johnmichael` (`id`, `name`, `username`, `email`, `password`) VALUES
+(0, 'mike', 'mike28', 'vmvichael23@gmail.com', '2345'),
+(0, 'zoren', 'zoren1', 'zoren1@gmail.com', '1234');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
