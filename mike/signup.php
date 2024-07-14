f613f0a55ac7dc28e16263b89b5f6a62dc665d0f
John Michael E. Vargas <129486648+JMichaelVargas@users.noreply.github.com>
2024-07-14 21:37:18 +0800

    Update signup.php

--- mike/signup.php
+++ mike/signup.php
@@ -9,8 +9,25 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="css/signup1.css">
+    <link rel="stylesheet" href="css/style1.css">
     <title>Signup</title>
+    <script>
+        function validatePassword() {
+            var password = document.getElementById("password").value;
+            var cpassword = document.getElementById("cpassword").value;
+            var passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/;
+
+            if (!passwordRegex.test(password)) {
+                alert("Password must be at least 8 characters long and include a mix of letters, numbers, and symbols.");
+                return false;
+            }
+            if (password !== cpassword) {
+                alert("Passwords do not match.");
+                return false;
+            }
+            return true;
+        }
+    </script>
 </head>
 <body>
 <div class="container">
@@ -18,7 +35,7 @@
             <div class="card-body">
                 <i class="fa-sharp fa-solid fa-graduation-cap"></i><h1>JMV</h1><br>
                 <h2 class="card-title">Create an Account</h2>
-                <form action="" method="post">
+                <form action="" method="post" onsubmit="return validatePassword()">
                     <div class="mb-3">
                         <label for="name" class="form-label">Name</label>
                         <input type="text" class="form-control" name="name" id="name" required>
@@ -46,32 +63,37 @@
                 </form>
             </div>
         </div>
-    </div>
-        <?php
-            if(isset($_POST['submit'])){
+    </div>                                                                                              
+    <?php
+        if(isset($_POST['submit'])){
             require '_dbcon.php';
             $name = $_POST['name'];
             $username = $_POST['username'];
             $email = $_POST['email'];
             $password = $_POST['password'];
             $cpassword = $_POST['cpassword'];
-            $duplicate = mysqli_query($connect, "SELECT * FROM vargas_johnmichael WHERE username = '$username' OR email = '$email'");
 
-            if(mysqli_num_rows($duplicate)>0){
-                echo
-                "<script>alert('Username or Email has Already Exist')</script>";
-            }else{
-                if($password ==  $cpassword){
-                    $query = "INSERT INTO vargas_johnmichael VALUES('', '$name', '$username', '$email', '$password')";
-                    mysqli_query($connect, $query);
-                    echo
-                    "<script>alert('Account Created!')</script>";
-                }else{
-                    "<script>alert('Password does not match!')</script>";
+            // Server-side validation for password strength
+            if (!preg_match('/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};\'":\\|,.<>\/?]).{8,}$/', $password)) {
+                echo "<script>alert('Password must be at least 8 characters long and include a mix of letters, numbers, and symbols.');</script>";
+            } else {
+                $duplicate = mysqli_query($connect, "SELECT * FROM vargas_johnmichael WHERE username = '$username' OR email = '$email'");
+
+                if(mysqli_num_rows($duplicate) > 0){
+                    echo "<script>alert('Username or Email has Already Exist');</script>";
+                } else {
+                    if($password == $cpassword){
+                        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+                        $query = "INSERT INTO vargas_johnmichael (name, username, email, password) VALUES ('$name', '$username', '$email', '$hashed_password')";
+                        mysqli_query($connect, $query);
+                        echo "<script>alert('Account Created!');</script>";
+                    } else {
+                        echo "<script>alert('Passwords do not match!');</script>";
+                    }
                 }
             }
         }
-        ?>        
-        <script src="js\main.js"></script>
+    ?>        
+    <script src="js/main.js"></script>
 </body>
 </html>
