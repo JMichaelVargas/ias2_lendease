1652340b1fe98cd37f50f875e7049e0269ecee98
John Michael E. Vargas <129486648+JMichaelVargas@users.noreply.github.com>
2024-07-14 21:37:00 +0800

    Update login.php

--- mike/login.php
+++ mike/login.php
@@ -1,50 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.0/css/bootstrap.min.css">
+    <!-- FONTAWESOME CSS ICON-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="css/signup1.css">
+    <link rel="stylesheet" href="css/style1.css">
     <title>Login</title>
 </head>
-
 <body>
 <div class="container">
         <div class="card">
             <div class="card-body">
-            <i class="fa-sharp fa-solid fa-graduation-cap"></i><h1>JMV</h1><br>
                 <h2 class="card-title">Account Login</h2>
                 <form action="" method="post" autocomplete="off">
                     <div class="mb-3">
                         <label for="usernameemail" class="form-label">Username or Email:</label>
-                        <input type="text" class="form-control" name="usernameemail" id="usernameemail" required value="">
+                        <input type="text" class="form-control" name="usernameemail" id="usernameemail" required>
                     </div>
                     <div class="mb-3">
                         <label for="password" class="form-label">Password:</label>
-                        <input type="password" class="form-control" name="password" id="password" required value="">
+                        <input type="password" class="form-control" name="password" id="password" required>
                         <label class="form-label" for="showPasswordCheckbox">Show Password</label>
                         <input type="checkbox" id="showPasswordCheckbox" onclick="togglePasswordVisibility()"><br><br>
                     </div>
+                    <div class="text-center">
+                        <a href="forgot_password.php">Forgot your password? Click here</a>
+                    </div><br>
                     <button type="submit" class="btn btn-primary" name="submit">Login</button>
                     <span>Don't have an account? <a href="signup.php">Register here</a></span>
                 </form>
             </div>
         </div>
+    </div>                                                                                              
     <?php
-    if (isset($_POST['submit'])) {
+    if(isset($_POST['submit'])){
         require '_dbcon.php';
         $usernameemail = $_POST['usernameemail'];
-        $password = $_POST['password']; // add isset() check here
-        $result = mysqli_query($connect, "SELECT * FROM vargas_johnmichael WHERE username = '$usernameemail' OR email = '$usernameemail'");
-        $row = mysqli_fetch_assoc($result);
+        $password = $_POST['password']; // Add isset() check here
 
-        if (mysqli_num_rows($result) > 0) {
-            if (!empty($row) && $password == $row['password']) {
+        // Use prepared statements to prevent SQL injection
+        $stmt = $connect->prepare("SELECT * FROM vargas_johnmichael WHERE username = ? OR email = ?");
+        $stmt->bind_param("ss", $usernameemail, $usernameemail);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+
+        if($row) {
+            if (password_verify($password, $row['password'])) {
+                session_start();
                 $_SESSION["login"] = true;
                 $_SESSION["id"] = $row["id"];
                 header("location: index.html");
@@ -54,8 +62,11 @@
         } else {
             echo "<script>alert('User not Registered');</script>";
         }
+
+        $stmt->close();
+        $connect->close();
     }
-    ?>
-    <script src="js\main.js"></script>
+    ?>        
+    <script src="js/main.js"></script>
 </body>
 </html>
