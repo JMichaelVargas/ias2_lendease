e4bf39962da54ef056b129f35ba0fde7cecd382f
John Michael E. Vargas <129486648+JMichaelVargas@users.noreply.github.com>
2024-07-23 20:17:25 +0800

    Update forgot_password.php

--- mike/forgot_password.php
+++ mike/forgot_password.php
@@ -2,18 +2,46 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style1.css">
     <title>Forgot Password</title>
+    <script>
+        function validatePassword() {
+            var newpassword = document.getElementById("newpassword").value;
+            var cnewpassword = document.getElementById("cnewpassword").value;
+            var passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]).{8,}$/;
+
+            if (!passwordRegex.test(newpassword)) {
+                alert("Password must be at least 8 characters long and include a mix of letters, numbers, and symbols.");
+                return false;
+            }
+            if (newpassword !== cnewpassword) {
+                alert("Passwords do not match.");
+                return false;
+            }
+            return true;
+        }
+
+        function togglePasswordVisibility() {
+            var newpassword = document.getElementById("newpassword");
+            var cnewpassword = document.getElementById("cnewpassword");
+            if (newpassword.type === "password" && cnewpassword.type === "password") {
+                newpassword.type = "text";
+                cnewpassword.type = "text";
+            } else {
+                newpassword.type = "password";
+                cnewpassword.type = "password";
+            }
+        }
+    </script>
 </head>
 <body>
     <div class="container">
         <div class="card">
             <div class="card-body">
                 <h2 class="card-title">Reset Password</h2>
-                <form action="" method="post">
+                <form action="" method="post" onsubmit="return validatePassword()">
                     <div class="mb-3">
                         <label for="usernameemail" class="form-label">Username or Email</label>
                         <input type="text" class="form-control" name="usernameemail" id="usernameemail" required>
@@ -25,6 +53,8 @@
                     <div class="mb-3">
                         <label for="cnewpassword" class="form-label">Confirm New Password</label>
                         <input type="password" class="form-control" name="cnewpassword" id="cnewpassword" required>
+                        <label class="form-label" for="showPasswordCheckbox">Show Password</label>
+                        <input type="checkbox" id="showPasswordCheckbox" onclick="togglePasswordVisibility()"><br><br>
                     </div>
                     <button type="submit" class="btn btn-primary" name="reset">Reset Password</button>
                     <span>Back to <a href="login.php" class="login">Login</a></span>
